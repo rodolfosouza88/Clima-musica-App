2512429e9a6f329ff424bb3ad0b3198ddf30a5f6
rodolfo <rodolfovasconcellos88@gmail.com>
2023-11-23 16:46:00 -0300

    criando funcoes de busca e alterando nome das cidades pelo JS

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Clima-musica-App 🌦️🎼
-Aplicativo de clima e tempo com as músicas mais tocadas de cada cidade pelo Spotify.
+Aplicativo de clima e tempo com as músicas mais tocadas de cada cidade usando API do Spotify
+e OpenWeather
 
 ## 💻 Demo
 https://app-climaemusica.netlify.app/
--- script.js
+++ script.js
@@ -1,33 +1,71 @@
-function btnBusca() {
-    const input = document.getElementById('input-busca').value;
+const input = document.getElementById('input-busca');
+const apiKey = '7834cf06368aec6780a2a133b91cf15f';
 
-    moveInput()
+
+function btnBusca() {
+    const inputValue = input.value;
+
+    moveInput(inputValue)
+}
+
+function moveInput(inputValue) {
+    const visibility = input.style.visibility;
+
+    inputValue && procurarCidade(inputValue);
+
+    visibility === 'hidden' ? abrirInput() : fecharInput();
 }
 
 function fecharInput() {
-    document.getElementById('input-busca').style.visibility = 'hidden';
-    document.getElementById('input-busca').style.width = '40px';
-    document.getElementById('input-busca').padding = '0.5rem 0.5rem 0.5rem 2.6rem';
-    document.getElementById('input-busca').style.transition = 'all 0.5s ease-in-out 0s';
-    document.getElementById('input-busca').value = "";
+    input.style.visibility = 'hidden';
+    input.style.width = '40px';
+    input.padding = '0.5rem 0.5rem 0.5rem 2.6rem';
+    input.style.transition = 'all 0.5s ease-in-out 0s';
+    input.value = "";
 }
 
 function abrirInput() {
-    document.getElementById('input-busca').style.visibility = 'visible';
-    document.getElementById('input-busca').style.width = '300px';
-    document.getElementById('input-busca').style.padding = '0.5rem 0.5rem 0.5rem 3.1rem';
+    input.style.visibility = 'visible';
+    input.style.width = '300px';
+    input.style.padding = '0.5rem 0.5rem 0.5rem 3.1rem';
+    input.style.transition = 'all 0.5s ease-in-out 0s';
+    input.value = "";
 }
 
-function moveInput() {
-    const visibility = document.getElementById('input-busca').style.visibility;
-
-    if (visibility === 'hidden') {
-        abrirInput()
-    } else {
-        fecharInput()
+input.addEventListener('keyup', function (event) {
+    if (event.keyCode === 13) {
+        const valorInput = input.value;
+        moveInput(valorInput)
     }
-}
+})
 
 document.addEventListener('DOMContentLoaded', () => {
     fecharInput();
 })
+
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+async function procurarCidade(city) {
+    try {
+        const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`)
+
+        if (dados.status === 200) {
+            const resultado = await dados.json();
+
+            mostrarClimaNaTela(resultado);
+            console.log(resultado, '<<')
+        } else {
+            throw new Error
+        }
+
+    } catch {
+        alert('A pesquisa por cidade deu errado!')
+    }
+}
+ 
+function mostrarClimaNaTela(resultado) {
+    document.querySelector('.nome-cidade').innerHTML = `${resultado.name}`;
+    document.querySelector('.temperatura').innerHTML = `${resultado.main.temp.toFixed(0)}°C`;
+    document.querySelector('.maxTemperatura').innerHTML = `MÁX: ${resultado.main.temp_max.toFixed(0)}°C`;
+    document.querySelector('.minTemperatura').innerHTML = `MIN: ${resultado.main.temp_min.toFixed(0)}°C`;
+}
